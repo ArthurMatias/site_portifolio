e635985f66ea9fd9c472398db0ee565111715ab2
Arthur M <arthurmatias2002@hotmail.com>
2024-01-31 00:15:06 -0300

    atualizado_com_scrollreveal

---
+++ scrollreveal.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.about',{
+    delay: 400
+})
+ScrollReveal().reveal('.projects',{
+    delay: 400
+})
+ScrollReveal().reveal('.html', {
+    delay: 400
+})
+ScrollReveal().reveal('.css',{
+    delay: 400
+})
+ScrollReveal().reveal('.js', {
+    delay: 400
+})
+ScrollReveal().reveal('.c', {
+    delay: 400
+})
